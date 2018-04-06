f0ce12dd8637d0ba02c0fbc752605f6d35320faf
Jana Siegle <jsiegle@lightpointdev.com>
2018-04-06 16:14:12 -0500

    more updates to intro
    Signed-off-by: Jana Siegle <jsiegle@lightpointdev.com>

--- resources/views/pages/intro.blade.php
+++ resources/views/pages/intro.blade.php
@@ -30,7 +30,10 @@
     <div class = "row-fluid">
     <div class="col-xs-12">
         <h4>What we can do for you.</h4>
-        <p>Need a website?  We are here for you.  Building and maintaining even a simple website can be a lot of work and it can be overwhelming; but... it doesn't need to be.  We are here to help you bring you website alive.  Working with a methodology of K. I. S. S. (Keep it simple) we will work with you for your perfect solution.  We don't automatically lock you into a maintenance contract if you don't want it or need it.  Want to host it on WordPress or SquareSpace, we can do it for you.  Want something unique and special for yourself?  We are up to the task!.  With pricing that works for any budget we will work with you to bring your vision, alive!
+        <p>Need a website?  We are here for you. </p>
+        <p> Building and maintaining even a simple website can be a lot of work and it can be overwhelming; but... it doesn't need to be.  We are here to help you bring your website alive.</p>
+        <p> Working with a methodology of K. I. S. S. (Keep it simple) we will work with you for your perfect solution.  We don't automatically lock you into a maintenance contract if you don't want it or need it.</p>
+        <p> Want to host it on WordPress or SquareSpace, we can do it for you.  Do you want something unique and special for yourself?  We are up to the task!.  With pricing that works for any budget we will work with you to bring your vision, alive!
         </p>
     </div>
     </div>
